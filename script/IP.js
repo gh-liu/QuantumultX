8d0f67d3a252a96c6bfb886251cfea7e3ccc132f
liugh7 <liugh.cs@gmail.com>
2021-03-16 16:55:16 +0800

    update

--- script/IP.js
+++ script/IP.js
@@ -10,7 +10,8 @@ if ($response.statusCode != 200) {
   var title = obj['country'];
   var subtitle = obj['city'] + ' ' + obj['isp'];
   var ip = obj['query'];
-  var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType'];
+  // var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType'];
+  var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
   
   
   $done({title, subtitle, ip, description});
